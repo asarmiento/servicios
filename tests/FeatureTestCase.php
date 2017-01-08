cef2da78b92858e7c0aa69a29b7fec754857f2c6
Anwar Sarmiento <asarmiento@sistemasamigables.com>
2017-01-08 13:47:32 -0600

    configuracion de unittest

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,15 @@
+<?php
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Created by PhpStorm.
+ * User: Amwar
+ * Date: 08/01/2017
+ * Time: 01:36 PM
+ */
+
+class FeatureTestCase extends TestCase
+{
+    use DatabaseTransactions;
+
+}
